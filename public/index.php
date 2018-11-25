387135ca9113753dd97c6abc37d617fc8dbfb723
inventivetalent <mail@inventivetalent.org>
2018-11-25 15:49:20 +0100

    fix favicon

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 <head>
     <title>WebRender - Take screenshots of any website</title>
     <meta name="description" content="Take screenshots of websites">
+    <link id="favicon" rel="shortcut icon" type="image/ico" href="favicon.ico">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link href="css/loading-bar.css" rel="stylesheet">
 </head>
